f158f24550d91200c23ba578cd4aaf8afec80f5f
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-06 13:32:44 +0530

    done

---
+++ sandwich.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class sandwich {
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter 3 digit number : ");
+    int num = sc.nextInt();
+    
+
+    if(num>=100 && num<1000)
+    {
+        int miiddiledigit = (num/100)%10;
+        if(miiddiledigit%3==0)
+        {
+          System.out.println("Trendy number ");
+        }
+        else 
+        {
+          System.out.println("Not a trendy Number");
+        }
+
+    }
+
+    else {
+      System.out.println("Invalid Number300");
+    }
+     
+  }
+  
+}
