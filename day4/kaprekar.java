090c8df618ecc5a34b4ef96e3104427e26737253
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-08 13:58:09 +0530

    done

---
+++ day4/kaprekar.java
@@ -0,0 +1,44 @@
+package day4;
+import java.util.*;
+public class kaprekar {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter digit : ");
+    int num = sc.nextInt();
+    int count = 0;
+    int sq = num*num;
+    int temp = num;
+
+    while(num!=0)
+    
+      {
+        
+         num/=10;
+        count++;
+       }
+
+       int power = (int) Math.pow(10,count);
+       int right = sq%power;
+       int left = sq/power;
+
+       if((left + right)==temp)
+       {
+          System.out.println("kaprekar Number");
+       }
+       else 
+       {
+        System.out.println("Not a kaprekar number");
+       }
+
+
+
+       
+
+
+  }
+
+
+
+  
+}
