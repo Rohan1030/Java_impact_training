e3f347f6b83ba4e49131aaa378ef3ae428ae456e
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-10 13:27:40 +0530

    done

---
+++ day6/cards.java
@@ -0,0 +1,21 @@
+package day6;
+import java.util.*;;
+public class cards {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  System.out.println("Enter number : ");
+  int n = sc.nextInt();
+
+   System.out.println("Enter the inserted element  : ");
+   int pos = sc.nextInt();
+
+   for(int index=0;index<size;index++)
+   {
+    if()
+   }
+
+
+
+  }
+  
+}
---
+++ day6/dublicate.java
@@ -0,0 +1,35 @@
+package day6;
+import java.util.*;
+
+public class dublicate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean found = false;
+        System.out.println("Duplicate numbers and their indices:");
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate number: " + arr[i] + " found at indices " + i + " and " + j);
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No duplicates found.");
+        }
+
+        sc.close();
+    }
+}
