f49471589ce68db809c66eb2713334f18b83b2fa
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-10 12:42:24 +0530

    done

---
+++ day5/array1d.java
@@ -0,0 +1,15 @@
+        package day5;
+        import java.util.*;
+
+        public class array1d {
+          public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n1 = sc.nextInt();
+            int arr1[] = new int [n1];
+            int n2 = sc.nextInt();
+
+            
+            
+          }
+          
+        }
