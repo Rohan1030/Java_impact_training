9edcf67e3eff89901762e469be21a33881d9b791
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-13 09:09:51 +0530

    done

---
+++ day7/arrays.java
@@ -0,0 +1,13 @@
+package day7;
+import java.util.*;
+public class arrays{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+       System.out.print("Enter digit : ");
+              int n = sc.nextInt();
+
+
+         
+  }
+  
+}
---
+++ day8/bus.java
@@ -0,0 +1,42 @@
+package day8;
+import java.util.*;
+public class bus {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter digit : ");
+    int n = sc.nextInt();
+
+
+     ArrayList<Integer> even = new ArrayList<>();
+     ArrayList<Integer> odd = new ArrayList<>();
+
+
+
+    int[] arr = new int[n];
+    System.out.println("Enter " + n+ " numbers:");
+              for (int i = 0; i < n; i++) {
+                  arr[i] = sc.nextInt();
+              }
+              
+              for(int i=0;i<n;i++)
+              {
+                if(arr[i]%2==0)
+                {
+
+                   even.add(arr[i]);
+
+                }
+                else 
+                {
+                  odd.add(arr[i]);
+                }
+                
+              }
+               even.addAll(odd);
+
+               System.out.println("Numbers (even first, then odd): " + even);
+
+  }
+  
+}
