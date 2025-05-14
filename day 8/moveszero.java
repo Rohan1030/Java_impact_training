a36b6128d4279a9df83be09e038545ab5482c9f0
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-14 11:28:26 +0530

    added

---
+++ day 8/moveszero.java
@@ -0,0 +1,5 @@
+package day 8;
+
+public class moveszero {
+  
+}
--- day8/evenodd.java
+++ day8/evenodd.java
@@ -41,3 +41,51 @@ public class evenodd {
   }
   
 }
+
+
+
+
+/*
+
+ using sliding window
+
+package day8;
+import java.util.*;
+
+public class evenodd {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter digit : ");
+    int n = sc.nextInt();
+
+    int[] arr = new int[n];
+    System.out.println("Enter " + n + " numbers:");
+    for (int i = 0; i < n; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    ArrayList<Integer> even = new ArrayList<>();
+    ArrayList<Integer> odd = new ArrayList<>();
+
+    // Simulating a sliding window of size 1
+    int windowStart = 0;
+    while (windowStart < n) {
+      int windowEnd = windowStart; // Window of size 1
+      int num = arr[windowEnd];
+      if (num % 2 == 0) {
+        even.add(num);
+      } else {
+        odd.add(num);
+      }
+      windowStart++; // Slide the window by 1
+    }
+
+    // Merge results
+    even.addAll(odd);
+
+    System.out.println("Numbers (even first, then odd): " + even);
+  }
+}
+ */
+
