8607d97945b4efc3644c367ae3dbb03ce5b3329c
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-20 12:49:19 +0530

    done

---
+++ day13/count.java
@@ -0,0 +1,23 @@
+package day13;
+import java.util.*;
+public class count {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a string : ");
+    String str = sc.nextLine();
+
+    int count =0;
+    for(int i=0;i<str.length();i++){
+      char ch = str.charAt(i);
+      if(ch =='a' || ch == 'A')
+      {
+        count++;
+      }
+
+
+   
+  }
+  System.out.println(count );
+  
+}
+}
