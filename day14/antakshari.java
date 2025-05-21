7939c295ecf1d6e90ef2b7c24dde05f5968a2078
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-21 13:14:44 +0530

    Done

---
+++ day14/antakshari.java
@@ -0,0 +1,27 @@
+package day14;
+import java.util.*;
+
+public class antakshari {
+  public static void main(String[] args) {
+  String w = "word";
+  String d = "Diameter";
+  String r = "run";
+
+  if(w.charAt(w.length()-1)==d.charAt(0)|| d.charAt(d.length()-1)==r.charAt(0))
+  {
+    System.out.println("word");
+    System.out.println("Diameter");
+    System.out.println("run");
+  }
+  else 
+  {
+    System.out.println("#######");
+  }
+
+    
+
+    
+    
+  }
+  
+}
