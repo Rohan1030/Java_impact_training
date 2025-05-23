738d4d50f3cf89ba63730aaca115175bc8f94a45
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-05-23 08:08:56 +0530

    done

---
+++ day16/games.java
@@ -0,0 +1,38 @@
+package day16;
+import java.util.*;
+
+public class games {
+public static void main(String[] args) {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter Number : ");
+  String a = sc.nextLine();
+  int sum =0;
+
+  for(int i=0;i<a.length();i++)
+  {
+   
+
+  }
+  
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+}  
+}
