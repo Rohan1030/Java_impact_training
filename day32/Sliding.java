5bdc407ed303ff979a88e4a1a2c710a809c2b09d
Rohan Kumar Sharma <rkrk02233@gmail.com>
2025-06-07 11:25:16 +0530

    ..

--- day32/Sliding.java
+++ day32/Sliding.java
@@ -2,7 +2,23 @@ package day32;
 import java.util.*;
 public class Sliding {
   public static void main(String[] args) {
+    int[] arr = {5,7,2,3,4};
+    int n = arr.length;
+    Arrays.sort(arr);
+    int sum =0;
     
+    for(int i=n-3;i<n;i++)
+    {
+      sum += arr[i];
+      System.out.println(arr[i]);
+      
+    }
+    System.out.println(sum);
+
+    
+
+
+
     
   }
   
